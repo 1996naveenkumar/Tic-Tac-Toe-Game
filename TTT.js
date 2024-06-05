c68d7bead8b05858151131cc7304a58d0eac021c
Naveen Kumar <naveennic7@gmail.com>
2024-06-05 13:43:26 +0530

    added a feature if match is draw

--- TTT.js
+++ TTT.js
@@ -1,8 +1,11 @@
 let boxes = document.querySelectorAll(".box");
 let newGameBtn = document.getElementById("newGame");
-let msgContainer = document.getElementById("msgContainer");
-let msg = document.getElementById("msg");
+let msgContainer1 = document.getElementById("msgContainer1");
+let msgContainer2 = document.getElementById("msgContainer2");
+let winnerMsg = document.getElementById("winnerMsg");
+let drawMsg = document.getElementById("drawMsg");
 
+let movesCount = 0;
 let turnO = true;
 
 const winPatterns = [
@@ -27,8 +30,8 @@ boxes.forEach((box) => {
             turnO = true;
             box.style.color = "red"
         }
-        console.log("box as clicked");
-     
+        console.log("Box clicked");
+        movesCount++;
         box.disabled = true;
         checkWinner();
     });
@@ -43,8 +46,10 @@ const enableBoxes = () => {
 
 let newGame = () => {
     turnO = true;
+    movesCount = 0;
     enableBoxes();
-    msgContainer.classList.add("hide");
+    msgContainer1.classList.add("hideWinner");
+    msgContainer2.classList.add("hideDraw");
 };
 
 const disableBoxes = () => {
@@ -54,26 +59,36 @@ const disableBoxes = () => {
 };
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
+    winnerMsg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer1.classList.remove("hideWinner");
+    disableBoxes();
+}
+
+const showDraw = () => {
+    drawMsg.innerText = "Match draw, start a new game!";
+    msgContainer2.classList.remove("hideDraw");
     disableBoxes();
 }
 
 let checkWinner = () => {
+    let winnerFound = false;
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
-
-        if (pos1Val != "" && pos1Val != "" && pos1Val != "") {
+        
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 console.log("winner", pos1Val);
                 showWinner(pos1Val);
-            } 
+                winnerFound = true;
+                break;
+            }
         } 
+    }
+    if (movesCount === 9 && !winnerFound) {
+        showDraw();
     }
 };
 
 newGameBtn.addEventListener("click", newGame);
-
-
